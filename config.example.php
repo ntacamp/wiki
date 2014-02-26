d7f5ecc56b7ba752fee07c000d2efe3f7809d9bb
Povilas Balzaravicius Pawka <pavvka@gmail.com>
2014-02-26 13:40:53 +0200

    Wiki

---
+++ config.example.php
@@ -0,0 +1,16 @@
+<?php
+## Database settings
+$wgDBtype = "mysql";
+$wgDBserver = "localhost";
+$wgDBname = "";
+$wgDBuser = "";
+$wgDBpassword = "";
+
+# MySQL specific settings
+$wgDBprefix = "";
+
+$wgSecretKey = "";
+
+# Site upgrade key. Must be set to a string (default provided) to turn on the
+# web installer while LocalSettings.php is in place
+$wgUpgradeKey = "";
